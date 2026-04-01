16fb0ea36abbbde86a24317c2fa2c4e6132f2908
Hamza Ansar <i256522@isb.nu.edu.pk>
2026-04-01 11:33:43 +0500

    first commit

---
+++ demo.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hamza ansar";
+    return 0;
+}
---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hamza is the best programmer in the world";
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    cout<<"the number you entered is "<<n;  
+    return 0;
+}
